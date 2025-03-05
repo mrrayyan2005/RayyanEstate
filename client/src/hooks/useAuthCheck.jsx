ea041c6706769fa87a9aa8891e48f7a474487483
Mohd Rayyan <143685393+mrrayyan2005@users.noreply.github.com>
2025-03-05 23:09:53 +0530

    Update useAuthCheck.jsx

--- client/src/hooks/useAuthCheck.jsx
+++ client/src/hooks/useAuthCheck.jsx
@@ -1,20 +1,26 @@
-import { useAuth0 } from '@auth0/auth0-react'
-import React from 'react'
-import { toast } from 'react-toastify';
-const useAuthCheck = () => {
-    const {isAuthenticated}=useAuth0();
-    const validateLogin=(user)=>{
-        if(!isAuthenticated){
-           toast.error("You must be logged in",{position:"bottom-right"});
-           return false;
-        }
-       else return true;
-    }
-  return (
-    {
-        validateLogin
-    }
-  )
-}
+import { useAuth0 } from "@auth0/auth0-react";
+import { toast } from "react-toastify";
 
-export default useAuthCheck
+const useAuthCheck = () => {
+  const { isAuthenticated, loginWithPopup, loginWithRedirect } = useAuth0();
+
+  const validateLogin = async () => {
+    if (!isAuthenticated) {
+      toast.error("You must be logged in", { position: "bottom-right" });
+
+      try {
+        await loginWithPopup(); // Try popup login first
+      } catch (error) {
+        console.error("Popup login failed, using redirect...", error);
+        await loginWithRedirect(); // Fallback to redirect login
+      }
+      return false;
+    }
+    return true;
+  };
+
+  return { validateLogin };
+};
+
+export default useAuthCheck;
+
