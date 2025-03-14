f3534d9408e9e87592903e7028fe39bbd7176e83
Mohd Rayyan <143685393+mrrayyan2005@users.noreply.github.com>
2025-03-14 12:13:48 +0530

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,21 +1,11 @@
 import express from "express";
 import jwtCheck from "../config/auth0Config.js";
-import {
-    getAllFavorites,
-    toFav,
-    getAllBookings,
-    createUser,
-    bookVisit,
-    cancelBooking
-} from "../controllers/userCntrl.js";
-
-const router = express.Router();
-
-router.post("/register", jwtCheck, createUser);
-router.post("/bookVisit/:id", jwtCheck, bookVisit);
-router.post("/allBookings", jwtCheck, getAllBookings);  // JWT applied
-router.post("/removeBooking/:id", jwtCheck, cancelBooking);
-router.post("/toFav/:rid", jwtCheck, toFav);
-router.post("/allFav", jwtCheck, getAllFavorites);  // JWT applied
-
-export { router as userRoute };
+import {getAllFavorites, toFav,getAllBookings,createUser ,bookVisit,cancelBooking} from "../controllers/userCntrl.js";
+const router=express.Router();
+router.post("/register",jwtCheck,createUser);
+router.post("/bookVisit/:id",jwtCheck,bookVisit)
+router.post("/allBookings",getAllBookings)
+router.post("/removeBooking/:id",jwtCheck,cancelBooking)
+router.post("/toFav/:rid",jwtCheck,toFav)
+router.post("/allFav",jwtCheck,getAllFavorites)
+export {router as userRoute}
