22bcbf97a84c062712de9d2463de5ad83307c74e
Mohd Rayyan <143685393+mrrayyan2005@users.noreply.github.com>
2025-03-14 16:11:02 +0530

    Update auth0Config.js

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import { auth } from 'express-oauth2-jwt-bearer';
 
 const jwtCheck = auth({
-    audience: 'https://rayyan-estate-server.vercel.app' || 'http://localhost:8000',  // Use deployed URL or localhost
+    audience: 'https://rayyan-estate-server.vercel.app',  // Use deployed URL or localhost
     issuerBaseURL: 'https://dev-7q83fq2272cfxooq.us.auth0.com/',
     tokenSigningAlg: 'RS256'
 });
