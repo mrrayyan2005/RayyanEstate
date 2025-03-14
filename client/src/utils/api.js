411256b07d661ecf9b559585460ff32a0528898e
Mohd Rayyan <143685393+mrrayyan2005@users.noreply.github.com>
2025-03-14 15:42:37 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import dayjs from "dayjs";
 import { toast } from "react-toastify";
 export const api = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://rayyan-estate-server.vercel.app/api",
   });
   export const getAllProperties = async () => {
     try {
